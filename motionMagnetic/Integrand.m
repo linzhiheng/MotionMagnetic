83f20bbd6f30237534b2718cb4da88b0825d760e
linzhiheng <linx-x@live.com>
2016-10-15 22:14:51 +0800

    更新

--- motionMagnetic/Integrand.m
+++ motionMagnetic/Integrand.m
@@ -1,9 +1,9 @@
-function Integrand = Integrand(Xi,Eta,EMField,unit,radius)
+function Integ = Integrand(Xi,Eta,EMField,unit,radius)
     %输入检查，输入的Xi和Eta需要在[-1,1]之间。
     validateattributes(Xi,{'numeric'},{'>=',-1,'<=',1},'Integrand','Xi',1);
     validateattributes(Eta,{'numeric'},{'>=',-1,'<=',1},'Integrand','Eta',2);
     validateattributes(EMField,{'numeric'},{'numel',3},'Integrand','EMField',3);
-    validateattributes(unit,{'struct'},{'size',[1,1]},'Integrand','unit',4);
+    validateattributes(unit,{'struct'},{'size',[1,1,1]},'Integrand','unit',4);
     validateattributes(radius,{'numeric'},{'numel',2},'Integrand','radius',5);
     %检查结束
     
@@ -37,9 +37,9 @@ function Integrand = Integrand(Xi,Eta,EMField,unit,radius)
         Cumulate(3) = Cumulate(3)+N(i)*( Vx(i)*Fy-Vz(i)*Fx )*x;
     end
     
-    Integrand = [0,0,0];
+    Integ = [0,0,0];
     for i = 1:3
-        Integrand(i) = Cumulate(i)*(J/R3);
+        Integ(i) = Cumulate(i)*(J/R3);
     end
     
 end
