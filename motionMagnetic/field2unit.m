7c0b00d0fe1ada1d33d9177b4c2afa1427b3c706
linzhiheng <linx-x@live.com>
2016-10-20 15:35:06 +0800

    更新

--- motionMagnetic/field2unit.m
+++ motionMagnetic/field2unit.m
@@ -1,4 +1,14 @@
 function unit = field2unit(field)
+    %输入检查，输入的p1和p2需要是一个含有两个元素的数组
+%     validateattributes(field,{'numeric'},{'ndims',3},'field2unit','field',1);
+    %检查结束
+    [nRow, nColumn, nd] = size(field);
+    unit(nRow-1,nColumn-1) = struct('velocity',[],'coords',[],'conductivity',[]);
     
-    unit = field;
+    for j = 1:nColumn-1
+        for i = 1:nRow-1
+            unit(i,j).velocity = [field(i, j, :); field(i+1, j, :); field(i+1, j+1, :); field(i, j+1, :)];
+        end
+    end
+%     unit = field;
 end
